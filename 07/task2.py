d872b300043882bda51d0b7c4b6e35675f6e994a
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2023-12-07 08:02:56 +0100

    cleaning

--- 07/task2.py
+++ 07/task2.py
@@ -1,81 +1,30 @@
-
 from day import Day
 from collections import defaultdict, Counter
-# from tqdm import tqdm
 
 day = Day(7)
 # day = Day(7, 'ex.in')
 
 
 lines = day.lines
-
-# time = [int(s) for s in lines[0].split(' ') if s.isdigit()]
-# distance = [int(s) for s in lines[1].split(' ') if s.isdigit()]
-
 hands = []
 for l in lines:
     hand, bid = l.split(' ')
     hands.append((hand, int(bid)))
 
 # print(hands)
-def full(x):
-    c = Counter(x)
-    if len(c.keys()) != 2:
-        return False
-    elif len(c.keys()) == 2:
-        vals = set(list(c.values()))
-        return vals == {2,3}
-    return False
-
-def three_of_a_kind(x):
-    c = Counter(x)
-    if len(c.keys()) != 3:
-        return False
-    elif len(c.keys()) == 3:
-        vals = set(list(c.values()))
-        return vals == {1,3}
-    return False
-
-def two_pair(x):
-    c = Counter(x)
-    if len(c.keys()) != 3:
-        return False
-    elif len(c.keys()) == 3:
-        vals = set(list(c.values()))
-        return vals == {1,2,2}
-    return False
-
-def five_of_a_kind(x):
-    return len(set(x)) == 1
-
-def four_of_a_kind(x):
-    return len(set(x)) == 2 and {1, 4}==set(Counter(x).values())
-
-def one_pair(x):
-    return len(set(x)) == 4
-
-def high_card(x):
-    return len(set(x)) == 5
-
 kinds_rules = {
-    # all five characters are the same
-    "Five of a kind" : five_of_a_kind,
-    # four of the five characters are the same
-    "Four of a kind": four_of_a_kind,
-    # three of the five characters are the same, but remaining two are also the same
-    "Full house": full,
-    "Three of a kind": three_of_a_kind,
-    "Two pair" : two_pair,
-    "One pair": one_pair,
-    "High card": high_card
+    "Five of a kind": lambda x: len(set(x)) == 1,
+    "Four of a kind": lambda x: len(set(x)) == 2 and {1, 4} == set(Counter(x).values()),
+    "Full house": lambda x: len(set(x)) == 2 and {2, 3} == set(Counter(x).values()),
+    "Three of a kind": lambda x: len(set(x)) == 3 and {1, 1, 3} == set(Counter(x).values()),
+    "Two pair": lambda x: len(set(x)) == 3 and {2, 1} == set(Counter(x).values()),
+    "One pair": lambda x: len(set(x)) == 4,
+    "High card": lambda x: len(set(x)) == 5,
 }
 
 kinds_ordering = [
-    # all five characters are the same
     "Five of a kind",
-    # four of the five characters are the same
     "Four of a kind",
-    # three of the five characters are the same, but remaining two are also the same
     "Full house",
     "Three of a kind",
     "Two pair",
@@ -98,20 +47,6 @@ for hand, bid in hands:
         if rule(newhand2):
             kinds[kind].append((hand, bid))
 
-# for hand, bid in hands:
-#     stronges = []
-#     for i, kind in enumerate(kinds_ordering):
-#         rule = kinds_rules[kind]
-#         newhand = hand.replace('J', '')
-#         c = Counter(newhand)
-#         torep = c.most_common(1)[0][0]
-#         newhand2 = hand.replace('J', torep)
-#         isthis = rule(hand)
-#         stronges.append((isthis, (hand,bid)))
-
-#     kinds[kind].append((hand, bid))
-print(kinds)
-
 def mykey(x):
     if x.isdigit():
         return int(x)
@@ -130,19 +65,12 @@ for kind, l in kinds.items():
     print(kind, sorted_kinds[kind])
 
 ordered = []
-broken = False
 for kind in reversed(kinds_ordering):
     if kind in sorted_kinds.keys():
         ordered.extend(sorted_kinds[kind])
-    else:
-        broken = True
 
 sums = []
 print(ordered)
 for i, (hand, bid) in enumerate(ordered):
     sums.append((i+1)*bid)
 print(sum(sums))
-print(broken)
-# day/sum(sums)
-#299175475
-#296679587
